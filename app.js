8ad831fe0cf560cc59ffacf1a921cd872c309c11
Ashymaliev <Nursultan-99kg@mail.ru>
2021-09-19 18:26:32 +0600

    add drag and drop

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 const item = document.querySelector('.item')
 
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+
+function dragstart(event){
+  event.target.classList.add('hold')
+  setTimeout(() =>event.target.classList.add
+  ('hide'),0)
+  
+
+}
+
+function dragend(event){
+   event.target.className = 'item'
+  
+
+}
